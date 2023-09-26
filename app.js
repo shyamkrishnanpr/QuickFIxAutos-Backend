bbb7d266858d56c8e03831ef2280b7be6f744649
shyam krishnan <shyamkrishnasby@gmail.com>
2023-09-26 13:21:34 +0530

    cors updated

--- app.js
+++ app.js
@@ -28,10 +28,19 @@ const corsOptions = {
   origin: allowedOrigins,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
-  exposedHeaders: ["Access-Control-Allow-Origin"]
+  
 };
 
 app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+
+  res.header('Access-Control-Allow-Origin', allowedOrigins);
+  res.header('Access-Control-Allow-Headers', true);
+  res.header('Access-Control-Allow-Credentials',true);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+});
 
 // Handle preflight (OPTIONS) requests for all routes
 app.options('*', cors(corsOptions));
