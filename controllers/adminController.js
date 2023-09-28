f12f2e6b9e4c0c7cf1b5f1c8f0e92e6fb66d220b
shyam krishnan <shyamkrishnasby@gmail.com>
2023-09-28 20:36:50 +0530

    dashboard admin final

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -525,11 +525,11 @@ const displayCharts = async (req, res, next) => {
         },
       },
     ];
-    
+
     const serviceChart = await serviceModel.aggregate(pipeLine);
     const bookingChart = await BookingModel.aggregate(pipeLine)
     const usersChart = await UserModel.aggregate(pipeLine)
-    const vendorChart = await VehicleModel.aggregate(pipeLine)
+    const vendorChart = await VendorModel.aggregate(pipeLine)
 
     res.json({
       serviceChart,
